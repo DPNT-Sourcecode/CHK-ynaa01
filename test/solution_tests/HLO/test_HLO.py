072115e1f4c4bc7b6514d77441bb6fd3525d25b6
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 17:24:01 +0000

    Mon Apr 08 17:24:01 UTC 2024

--- test/solution_tests/HLO/test_HLO.py
+++ test/solution_tests/HLO/test_HLO.py
@@ -4,7 +4,7 @@ from lib.solutions.HLO.hello_solution import HelloFriend
 
 @pytest.fixture()
 def hello_friend_fixture():
-    return HelloFriend
+    return HelloFriend()
 
 def test_hello_friend(hello_friend_fixture):
     assert hello_friend_fixture.hello("James") == "Hello, James!"
