dac8eeed192dd0b1c3172c7422461d0ffd792bcf
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 21:16:33 +0000

    Mon Apr 08 21:16:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,8 +17,21 @@ def checkout(skus):
     decrease_B_count_offer = entered_skus['E'] // 2
 
     for sku, count in entered_skus.items():
-        
+        if sku == 'A':
+            quotient, remainder = divmod(count, 3)
+            total_price += quotient *130 + remainder * prices[sku]
+        elif sku == 'B':
+            if count > 0:
+                count -= decrease_B_count_offer
+                if count < 0:
+                    count = 0
+            quotient, remainder = divmod(count, 2)
+            total_price += quotient*45 + remainder * prices['B']
+        else:
+            total_price += prices[sku] * count
+
 
     return total_price
+
 
 
