94764bae27974898041827e9d2927a7c8711000d
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 17:38:36 +0000

    Mon Apr 08 17:38:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,6 +6,21 @@ def checkout(skus):
     #  Dict to store prices
     prices = {'A':50, 'B':30, 'C':20, 'D':15}
     special_offers = {'A': [(3, 130)], 'B':[(2, 45)]}
-    
+
+    #  Dict to store counts of each SKU
+    counts = count_skus(skus)
+
+    #  Calc total price
+    total_price = calc_total_price(prices, special_offers, counts)
+
+    return total_price
+
+def count_skus(skus):
+    counts = {sku: 0 for sku in 'ABCD'}
+    for sku in skus:
+        counts[sku] += 1
+    else:
+        return None
+
 
 
