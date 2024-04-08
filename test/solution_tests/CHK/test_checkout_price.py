a0ca4bb05ccd0b754fb9cf17eefa56f8d2261be6
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 18:02:36 +0000

    Mon Apr 08 18:02:36 UTC 2024

--- test/solution_tests/CHK/test_checkout_price.py
+++ test/solution_tests/CHK/test_checkout_price.py
@@ -14,4 +14,13 @@ def test_count_skus(skus, expected_counts):
     ({'A': 50, 'B':30, 'C':20, 'D':15}, {'A':[(3, 130)], 'B':[(2, 45)]}, {'A':4, 'B':2, 'C':1, 'D':1}, 195),
     ({'A': 50, 'B':30, 'C':20, 'D':15}, {'A':[(3, 130)], 'B':[(2, 45)]}, {'A':3}, 130)
 ])
-def
+def test_calculate_total_price(prices, special_offers, counts, expected_total):
+    assert calc_total_price(prices, special_offers, counts) == expected_total
+
+@pytest.mark.parametrize("skus, expected_total", [
+    ("ABCDABAA", 195),
+    ("ABE", -1),
+    ("AAA", 130)
+])
+def test_checkout(skus, expected_total):
+    assert checkout(skus) == expected_total
