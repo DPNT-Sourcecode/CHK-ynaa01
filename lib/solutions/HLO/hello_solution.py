f58ce30e0a4031bbb2be58b8b94f35d8762005f4
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 17:23:20 +0000

    Mon Apr 08 17:23:20 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 class HelloFriend:
-    def hello(self, friend_name):
+    def hello(self, friend_name: str):
         return f"Hello, {friend_name}!"
 
 
--- test/solution_tests/HLO/test_HLO.py
+++ test/solution_tests/HLO/test_HLO.py
@@ -1,3 +1,10 @@
 import pytest
 
-from lib.solutions.HLO
+from lib.solutions.HLO.hello_solution import HelloFriend
+
+@pytest.fixture()
+def hello_friend_fixture():
+    return HelloFriend
+
+def test_hello_friend(hello_friend_fixture):
+    assert hello_friend_fixture.hello("James") == "Hello, James!"
