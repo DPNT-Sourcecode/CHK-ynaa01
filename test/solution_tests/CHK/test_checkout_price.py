b982294c520ac3b3eba5fa168441e93bd6dd5099
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 18:17:36 +0000

    Mon Apr 08 18:17:36 UTC 2024

--- test/solution_tests/CHK/test_checkout_price.py
+++ test/solution_tests/CHK/test_checkout_price.py
@@ -26,3 +26,12 @@ from lib.solutions.CHK.checkout_solution import checkout
 # def test_checkout(skus, expected_total):
 #     assert checkout(skus) == expected_total
 
+def test_checkout_valid_input():
+    assert checkout("ABCDABAA") == 260
+    assert checkout("AAA") == 130
+    assert checkout("C") == 20
+
+def test_checkout_invalid_input():
+    assert checkout("E") == -1
+    assert checkout("") == 0
+    assert checkout("ABABABABAY") == -1
