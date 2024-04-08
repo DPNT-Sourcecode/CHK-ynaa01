0ee3bec53d4c29c850df7de3338f1b327fe1d43b
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 18:59:43 +0000

    Mon Apr 08 18:59:43 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@ def checkout(skus):
     #  Dict to store prices
     prices = {'A':50, 'B':30, 'C':20, 'D':15, 'E':40}
     #  OFfer for 5 As applied before the offer for 3 As
-    special_offers = {'A': [(5, 200), (3, 130)], 'B':[(2, 45)], 'E':[(2, prices['B'])]}
+    special_offers = {'A': [(5, 200), (3, 130)], 'B':[(2, 45)], 'E':[(2, prices['E'] + prices['B'])]}
 
     #  Count occurence of each sku
     sku_counts = {sku:0 for sku in prices}
@@ -11,6 +11,12 @@ def checkout(skus):
         if sku not in prices:
             return -1
         sku_counts[sku] += 1
+
+    if 'E' in sku_counts and sku_counts['E'] >= 2 and 'B' in sku_counts:
+        #  Calc number of free 'B' items
+        num_free_b = sku_counts['E'] // 2
+        # deduct price of B from total price for each free 'B'
+        total_price -= num_free_b * prices['B']
 
     #  Calc total price
     total_price = 0
@@ -24,6 +30,7 @@ def checkout(skus):
         total_price += sku_counts[sku] * prices[sku]
 
     return total_price
+
 
 
 
