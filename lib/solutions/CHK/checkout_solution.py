88b055cc9da0a9badebe978f6a1d2e801542b310
sbx_user1051 <sbx_user1051@169.254.61.125>
2024-04-08 18:11:36 +0000

    Mon Apr 08 18:11:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -52,5 +52,10 @@
 #         total_price += count * offer_price
 #         return total_price
 #
+def checkout(skus):
+    #  Dict to store prices
+    prices = {'A':50, 'B':30, 'C':20, 'D':15}
+    special_offers = {'A': [(3, 130)], 'B':[(2, 45)]}
+
 
 
